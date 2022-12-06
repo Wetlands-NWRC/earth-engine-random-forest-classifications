55f5bcd2e991c5260a617fc727f08d666966d361
Ryan Hamilton <ryangilberthamilton@gmail.com>
2022-12-06 10:04:46 -0500

    update target dir and removed output attribute

--- 001-williston/williston-region/001-geotiff-prep/config.py
+++ 001-williston/williston-region/001-geotiff-prep/config.py
@@ -3,6 +3,5 @@ from dataclasses import dataclass
 
 @dataclass
 class CFG:
-    tiles: str = r"P:\projects\williston-testing\reg01"
+    tiles: str = r"X:\archiveImagery\sentinel\sen2comp_bc_v01\reg01_williston_fill01avg\reg01"
     pattern: str = "**/*.tif"
-    output: str = "./dump"
