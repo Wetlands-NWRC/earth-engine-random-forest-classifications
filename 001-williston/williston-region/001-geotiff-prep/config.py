f199c3d70e659794c4dd59116195c08a025c33bb
Ryan Hamilton <ryangilberthamilton@gmail.com>
2022-12-06 08:19:32 -0500

    fix spelling mistake

--- 001-williston/williston-region/001-geotiff-prep/config.py
+++ 001-williston/williston-region/001-geotiff-prep/config.py
@@ -3,5 +3,5 @@ from dataclasses import dataclass
 
 @dataclass
 class CFG:
-    tile: str = r"X:\archiveImagery\sentinel\sen2comp_bc_v01\reg01_williston_fill01avg\reg01"
-    patter: str = "**/*.tif"
+    tiles: str = r"X:\archiveImagery\sentinel\sen2comp_bc_v01\reg01_williston_fill01avg\reg01"
+    pattern: str = "**/*.tif"
